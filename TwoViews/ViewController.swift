800a6b60964a337f8353968b91122e2315ff787b
Ruslan Shigapov <ilgamovich@gmail.com>
2022-09-14 11:46:13 +0300

    Initial Commit

---
+++ TwoViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoViews
+//
+//  Created by Руслан Шигапов on 14.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
